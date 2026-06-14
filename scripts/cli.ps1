8d2ff667dcfa72b9166350092609e14cfb9f95ac
Martin <qule520@126.com>
2026-06-15 00:28:35 +0800

    feat: implement background CLI server with Swagger integration and OpenAPI schema filtering

--- scripts/cli.ps1
+++ scripts/cli.ps1
@@ -5,7 +5,7 @@ $SolutionRootPath = Resolve-Path $(Join-Path $PSScriptRoot "..\")
 $ExePath = Resolve-Path $(Join-Path $PSScriptRoot "..\src\AgentExcel\bin\Debug\net10.0-windows7.0\win-x64\AgentExcel.exe")
 $DllPath = Resolve-Path $(Join-Path $PSScriptRoot "..\src\AgentExcel\bin\Debug\net10.0-windows7.0\win-x64\AgentExcel.dll")
 $BuildScript = Resolve-Path $(Join-Path $PSScriptRoot "build.ps1")
-$LastSourceUpdateTime = $(Get-ChildItem -File -Recurse -Filter "*.cs" -Path $SolutionRootPath | Sort-Object LastWriteTime -Descending | Select-Object -First 1).LastWriteTime
+$LastSourceUpdateTime = $(Get-ChildItem -File -Recurse -Filter "*.cs" -Path $SolutionRootPath |  Where-Object { $_.FullName -notlike '*\obj\*' -and $_.FullName -notlike '*\bin\*' } | Sort-Object LastWriteTime -Descending | Select-Object -First 1).LastWriteTime
 $LastBuildTime = $(Get-Item $DllPath).LastWriteTime
 $IsStop = $false
 if ($args.Count -gt 0 -and $args[0] -eq "stop") {
